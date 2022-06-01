785dcf0767dcd8e7b5d4dca7abe70e04b82dd648
Salman Khondker <salmankhondker1998@gmail.com>
2022-06-01 21:01:56 +0600

    converted array to dw. Now inputs and outputs negative numbers

--- inssort_binsearch.asm
+++ inssort_binsearch.asm
@@ -6,19 +6,22 @@
     LF DB 10
     CR DB 13    
     
-    ARRAY DB 100 DUP(0)     ; intels 8086 architectures supported registers are bx, si, di, and bp only.
+    ARRAY DW 100 DUP(0)     ; intels 8086 architectures supported registers are bx, si, di, and bp only.
     ARR_MAX_SZ DW 100 
     CURR_SZ DW 0
     NUM_INTS DW 0
+    NUM_SIGN_FLAG DB 0
     STRING DB "NOT FOUND$"
 .CODE
-
+                                ; PRINTING NEWLINE USES AH, WHICH MODIFIES AX, CHECK FOR THOSE BUGS
 
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
     
     CALL INPUT_INT_IN_AX
+    MOV BX, 2
+    MUL BX
     MOV NUM_INTS, AX
 
     CALL TAKE_NUM_INTS_INPUTS_AND_PUT_IN_ARRAY
@@ -31,13 +34,11 @@ MAIN PROC
     CALL PRINT_NEWLINE
     CALL PRINT_ARRAY 
 
-    CALL PRINT_NEWLINE
     CALL BIN_SEARCH_INPUT_LOOP        
     
     MOV AH, 4CH
     INT 21H                    
 ENDP
-
 
 
 
@@ -52,27 +53,43 @@ ENDP
 INPUT_INT_IN_AX PROC            ; MODIFIES AX, BX, CX 
     MOV BX, 0                   ; TEMP INPUT STORAGE
     MOV CX, 0                   ; ACCUMULATION
+                                ; INITIAL INPUT, IF -, NEG AT THE END
+    CALL INPUT_CHAR_IN_AL
+
+    CMP AL, '-'
+    JNE POS_NUM
+    PUSH 1                      ; FLAG TO NEG IN THE END
+    CALL INPUT_CHAR_IN_AL
+    JMP INPUT_START
+
+    POS_NUM:
+    PUSH 0
     
     INPUT_START:
-    CALL INPUT_CHAR_IN_AL
-         
                                 ; IF INPUT AT AL IS EQUAL TO LF, END INPUT
     CMP AL, CR
     JZ INPUT_END
-    
                                 ; CONVERT TO NUMBER
-    SUB AL, 48      
-    
-                                ; BX = 00[AL], AX = 0010, AX = AX * CX + BX, CX = AX. 
-    MOV BL, AL      
+    MOV AH, 0
+    SUB AL, '0'     
+                                ; BX = AX, AX = 0010, AX = AX * CX + BX, CX = AX. 
+    MOV BX, AX      
     MOV AX, 10           
     MUL CX                  
     ADD AX, BX          
     MOV CX, AX         
-      
+
+    CALL INPUT_CHAR_IN_AL
     JMP INPUT_START
     
     INPUT_END:
+    POP BX
+    CMP BX, 0
+    JE MOVE_RES_IN_AX
+
+    NEG CX
+
+    MOVE_RES_IN_AX:
     MOV AX, CX
     RET                           
 ENDP 
@@ -86,10 +103,10 @@ TAKE_NUM_INTS_INPUTS_AND_PUT_IN_ARRAY PROC      ; MODIFIES SI, CURR_SZ, AX, BX, 
     JGE STOP_INPUT
 
     CALL PRINT_NEWLINE
-
     CALL INPUT_INT_IN_AX
-    MOV ARRAY[SI], AL           ; DISCARDING HIGHER INPUT BYTE
-    INC SI
+
+    MOV ARRAY[SI], AX
+    CALL WORDSIZE_INC_SI
     CALL INC_ARRAY_CURRENT_SIZE
 
     JMP INPUT_LOOP
@@ -103,17 +120,15 @@ BIN_SEARCH_INPUT_LOOP PROC          ; MODIFIES AX, BX, BP, SI, DI
     TAKE_INPUT:
     CALL PRINT_NEWLINE
     CALL INPUT_INT_IN_AX
-    MOV BX, AX
 
-    CALL PRINT_NEWLINE
-    MOV AX, BX
     CALL BINARY_SEARCH_INT_IN_AX_INDEX_IN_SI
+    CALL PRINT_NEWLINE
 
     CMP SI, 0
     JL NOT_FOUND
     
-    FOUND:
-    MOV AX, SI
+    MOV AX, SI                      ; FOUND
+    SHR AX, 1
     CALL PRINT_INT_IN_AX
 
     JMP TAKE_INPUT
@@ -133,26 +148,40 @@ ENDP
 ; OUTPUTS         
 PRINT_INT_IN_AX PROC            ; MODIFIES AX, BX, CX, DX   
     MOV CX, 0                   ; NUM DIGITS
+    MOV BX, 10                  ; DIVISOR
+
+    TEST AX, AX                 ; IF AX IS NEG, SIGN BIT FLAG IS 1
+    JNS POSITIVE_NUM
+    MOV NUM_SIGN_FLAG, 1
+    NEG AX
+    JMP OUTPUT_STACK_START
+
+    POSITIVE_NUM:
+    MOV NUM_SIGN_FLAG, 0
+
     OUTPUT_STACK_START:      
     INC CX
     
-    MOV BX, 10                  ; BX = 0010, AL = AX / BL, AH = AX % BL
-    DIV BL                       
+    MOV DX, 0                   ; BX = 0010, DX = 0, AX = (DX:AX) / BX, DX = (DX:AX) % BX
+    DIV BX                      ; DISCARDING HIGHER 2 BYTES, AND ONLY CONSIDERING 2 BYTE DIVIDENDS
                                 
-    MOV BL, AL                  ; BX = 00[AL], AX = 00[AH], PUSH AX, AX = BX 
-    MOV AL, AH
-    MOV AH, 0  
-    PUSH AX    
-    MOV AL, BL  
+    PUSH DX                     ; PUSH REMAINDER
             
-    CMP AL, 0                   ; NO MORE PRINT
+    CMP AX, 0                   ; NO MORE PRINT
     JNE OUTPUT_STACK_START               
     
+    CMP NUM_SIGN_FLAG, 1
+    JNE STACK_PRINT_LOOP
+    MOV DX, -3                  ; ADDING '0' GIVES '-'
+    PUSH DX
+    INC CX
+
     STACK_PRINT_LOOP:           ; POP INTO DX (ASSUMING 1 BYTE DIG), MAKE DL ASCII, PRINT DX
     POP DX
-    ADD DL, 48      
+    ADD DL, '0'      
     CALL PRINT_CHAR_IN_DL 
     LOOP STACK_PRINT_LOOP
+
     RET        
 ENDP
 
@@ -170,14 +199,6 @@ PRINT_NEWLINE PROC              ; MODIFIES AH, DL
 ENDP            
 
 
-PRINT_SPACE PROC                ; MODIFIES AH, DL
-    MOV DL, 32
-    CALL PRINT_CHAR_IN_DL   
-    
-    RET
-ENDP   
-
-
 PRINT_CHAR_IN_DL PROC           ; MODIFIES AH
     MOV AH, 2
     INT 21H
@@ -192,13 +213,12 @@ PRINT_ARRAY PROC                ; MODIFIES SI, DL, AX
     CMP SI, CURR_SZ
     JGE END_PRINT
     
-    MOV DL, " "
+    MOV DL, ' '
     CALL PRINT_CHAR_IN_DL
           
-    MOV AL, ARRAY[SI]
-    MOV AH, 0
+    MOV AX, ARRAY[SI]
     CALL PRINT_INT_IN_AX
-    INC SI 
+    CALL WORDSIZE_INC_SI
 
     JMP TRAVERSE
     
@@ -216,10 +236,50 @@ ENDP
 
 
 
-           
+
+; ARRAY UTILS
+WORDSIZE_INC_SI PROC
+    INC SI
+    INC SI
+    RET
+ENDP
+
+
+WORDSIZE_DEC_SI PROC
+    DEC SI
+    DEC SI
+    RET
+ENDP
+
+
+WORDSIZE_INC_DI PROC
+    INC DI
+    INC DI
+    RET
+ENDP
+
+
+WORDSIZE_DEC_DI PROC
+    DEC DI
+    DEC DI
+    RET
+ENDP
+
+
+INC_ARRAY_CURRENT_SIZE PROC     ; MODIFIES DX, CURR_SZ VARIABLE.              
+    MOV DX, CURR_SZ
+    INC DX
+    INC DX
+    MOV CURR_SZ, DX
+    RET
+ENDP
+
+
+
+
 ; INSERTION SORT
-INSERTION_SORT PROC         ; MODIFIES SI, CX, DI, DX, ARRAY          
-    MOV SI, 1             ; RUN INSERTION SORT ON CONSTRUCTED ARRAY, ARRAY INDEXED 0, CURR_SZ-1.    
+INSERTION_SORT PROC             ; MODIFIES SI, CX, DI, DX, ARRAY          
+    MOV SI, 2                   ; RUN INSERTION SORT ON CONSTRUCTED ARRAY, ARRAY INDEXED 0, CURR_SZ-1.    
     
     INSERTION_LOOP:
     CMP SI, CURR_SZ
@@ -228,7 +288,7 @@ INSERTION_SORT PROC         ; MODIFIES SI, CX, DI, DX, ARRAY
     MOV CX, SI          
     CALL INSERT_INT_IN_ARRAY_SI_IN_SORTED_POSITION
     MOV SI, CX
-    INC SI
+    CALL WORDSIZE_INC_SI
 
     JMP INSERTION_LOOP
 
@@ -240,29 +300,24 @@ ENDP
 
 
 ; INSERTION SORT UTILS
-INC_ARRAY_CURRENT_SIZE PROC         ; MODIFIES DX, CURR_SZ VARIABLE.              
-    MOV DX, CURR_SZ
-    INC DX
-    MOV CURR_SZ, DX
-    RET
-ENDP
+
 
 
 INSERT_INT_IN_ARRAY_SI_IN_SORTED_POSITION PROC         ; MODIFIES SI, DI, DX AND ARRAY
                                     ; BUBBLE INT IN ARRAY[SI] TO IT'S SORT POSITION
     BUBBLE:
-    CMP SI, 1
+    CMP SI, 2
     JL STOP
     
     MOV DI, SI
-    DEC DI  
+    CALL WORDSIZE_DEC_DI 
      
-    MOV DL, ARRAY[SI]
-    CMP ARRAY[DI], DL
+    MOV DX, ARRAY[SI]
+    CMP ARRAY[DI], DX
     JLE STOP
     
     CALL SWAP_INTS_IN_ARRAY
-    DEC SI
+    CALL WORDSIZE_DEC_SI
     JMP BUBBLE
     
     STOP:
@@ -271,10 +326,12 @@ ENDP
 
 
 SWAP_INTS_IN_ARRAY PROC             ; MODIFIES DX, ARRAY[SI], ARRAY[DI]
-    MOV DL, ARRAY[SI]               ; SWAP VALUES OF ARRAY AT OFFSET SI AND DI
-    MOV DH, ARRAY[DI]
-    MOV ARRAY[SI], DH
-    MOV ARRAY[DI], DL 
+    MOV DX, ARRAY[SI]               ; SWAP VALUES OF ARRAY AT OFFSET SI AND DI
+    XOR ARRAY[DI], DX
+    MOV DX, ARRAY[DI]
+    XOR ARRAY[SI], DX
+    MOV DX, ARRAY[SI]
+    XOR ARRAY[DI], DX
     
     RET   
 ENDP
@@ -283,10 +340,8 @@ ENDP
 
 
 ; BIN SEARCH
-BINARY_SEARCH_INT_IN_AX_INDEX_IN_SI PROC        ; MODIFIES AX, BX, BP, SI, CL, DI
-    MOV CL, 2
-    MOV BL, AL                      ; DISCARDING INT HIGHER BYTE
-
+BINARY_SEARCH_INT_IN_AX_INDEX_IN_SI PROC        ; MODIFIES AX, BX, BP, SI, CX, DI
+    MOV BX, AX                      
     MOV SI, 0
     MOV DI, CURR_SZ
 
@@ -294,29 +349,34 @@ BINARY_SEARCH_INT_IN_AX_INDEX_IN_SI PROC        ; MODIFIES AX, BX, BP, SI, CL, D
     CMP SI, DI                      ; WHILE LEFT <= RIGHT
     JG NOT_IN_ARRAY
 
-    MOV AX, SI                  ; AX = (SI + DI) / 2
-    ADD AX, DI  
-    DIV CL
-    MOV AH, 0
-    MOV CX, SI
+    MOV AX, SI                      ; AX = (SI + DI) / 2
+    ADD AX, DI
+    SHR AX, 1
+    
+    TEST AX, 1                      ; MAKING INDEX EVEN FOR WORD SIZED INDEX
+    JZ EVEN
+    DEC AX
+    
+    EVEN:
+
+    MOV CX, SI                      ; TO INDEX
     MOV SI, AX
 
-    CMP ARRAY[SI], BL           ; ONLY SI, DI ARE INDEXING REGISTERS
+    CMP ARRAY[SI], BX               ; ONLY SI, DI ARE INDEXING REGISTERS
     MOV SI, CX
-    MOV CL, 2
     JL MOVE_RIGHT
     JG MOVE_LEFT
     JMP EQUAL
 
     MOVE_RIGHT:
     MOV SI, AX
-    INC SI
+    CALL WORDSIZE_INC_SI
 
     JMP ITERATION
 
     MOVE_LEFT:
     MOV DI, AX
-    DEC DI
+    CALL WORDSIZE_DEC_DI
 
     JMP ITERATION
 
@@ -333,9 +393,5 @@ BINARY_SEARCH_INT_IN_AX_INDEX_IN_SI PROC        ; MODIFIES AX, BX, BP, SI, CL, D
     RET
 ENDP
 
-
-
-; BIN SEARCH UTILS
-   
 
 END MAIN
